dc21df0231e7f8e0ceb7fa0f629559ca1cb868a8
pranaypatel512 <www.pranay1337.com@gmail.com>
2022-03-27 15:20:44 +0530

    Initial commit
    Signed-off-by: pranaypatel512 <www.pranay1337.com@gmail.com>

---
+++ app/src/androidTest/java/com/pranay/material3example/ExampleInstrumentedTest.kt
@@ -0,0 +1,22 @@
+package com.pranay.material3example
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.pranay.material3example", appContext.packageName)
+    }
+}
